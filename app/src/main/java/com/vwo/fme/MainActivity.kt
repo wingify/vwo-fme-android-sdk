d1f5d9d59249d942e309a9c1c9a98f42f6863d85
Swapnil Chaudhari <swapnil.chaudhari@wingify.com>
2024-07-10 17:32:19 +0530

    Add view binding in activity

--- app/src/main/java/com/vwo/fme/MainActivity.kt
+++ app/src/main/java/com/vwo/fme/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.vwo.fme
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.vwo.fme.databinding.ActivityMainBinding
+import com.vwo.sdk.fme.Calculator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnAdd.setOnClickListener {
+            /*val calculator = Calculator()
+            val result = calculator.add(2, 5)
+            Toast.makeText(this, "Result: $result", Toast.LENGTH_SHORT).show()*/
         }
     }
 }
