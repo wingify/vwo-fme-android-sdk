cb7eb8e49df4922a8a5bcf9f56713fb8b2165feb
Swapnil Chaudhari <swapnil.chaudhari@wingify.com>
2024-07-22 14:36:05 +0530

    Added test code

--- app/src/main/java/com/vwo/fme/MainActivity.kt
+++ app/src/main/java/com/vwo/fme/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.vwo.fme
 
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.vwo.VWO
 import com.vwo.fme.databinding.ActivityMainBinding
-import com.vwo.sdk.fme.Calculator
+import com.vwo.interfaces.IVwoInitCallback
+import com.vwo.models.user.VWOContext
+import com.vwo.models.user.VWOInitOptions
+
+
+private const val SDK_KEY = ""
+private const val ACCOUNT_ID = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.btnAdd.setOnClickListener {
-            val calculator = Calculator()
+            // Initialize VWO SDK
+            val vwoInitOptions = VWOInitOptions()
+            // Set SDK Key and Account ID
+            vwoInitOptions.sdkKey = SDK_KEY
+            vwoInitOptions.accountId = ACCOUNT_ID
+
+            vwoInitOptions.logger = mutableMapOf<String, Any>().apply { put("level","TRACE") }
+            // create VWO instance with the vwoInitOptions
+            VWO.init(vwoInitOptions, object : IVwoInitCallback {
+                override fun vwoInitSuccess(vwo: VWO, message: String) {
+                    Log.d("SwapnilFlag", "vwoInitSuccess $message")
+                    //getFlag(vwo)
+                }
+
+                override fun vwoInitFailed(message: String) {
+                    Log.d("SwapnilFlag", "vwoInitFailed: $message")
+                }
+            })
+
+
+            /*val calculator = Calculator()
             val result = calculator.add(2, 5)
             Toast.makeText(this, "Result: $result", Toast.LENGTH_SHORT).show()
         }
